934a34ef6dfecc7fe59b87a8c41f259ec8bd5fd1
DESKTOP-S9HU5A9\T <sample@yahoo.co.jp>
2026-04-11 11:28:45 +0900

    クロスオリジン変更

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -15,7 +15,8 @@ import com.example.demo.service.LoginService;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin("http://localhost:3000") // https://todo-list-plus-front-hironobu0706-hironobu0706s-projects.vercel.app/
+@CrossOrigin(origins = "*") // このコントローラーの全メソッドを全許可
+//@CrossOrigin("http://localhost:3000") // https://todo-list-plus-front-hironobu0706-hironobu0706s-projects.vercel.app/
 public class LoginController {
 
 	@Autowired
