51e11e4cd3c7180e4abda34997b4b9b5a78adf4f
DESKTOP-S9HU5A9\T <sample@yahoo.co.jp>
2026-08-10 10:04:47 +0900

    junit導入

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM eclipse-temurin:17
 WORKDIR /app
 COPY . .
 RUN chmod +x mvnw
-CMD ["./mvnw", "spring-boot:run"]
---
+++ src/test/java/com/example/demo/SampleTest1.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest1 {
+
+	@Test
+	void test() {
+		assertEquals(1,1);
+	}
+
+}
